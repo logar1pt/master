442a646f3f4c8729a82464c60a40bc704fcc8566
Logar1pt <osokin@gmail.com>
2016-05-30 02:41:26 +0400

    add new file for server

--- RegScript.js
+++ RegScript.js
@@ -5,9 +5,14 @@ if (!xmlHttp) {
 function request () {
     if (xmlHttp.readyState == 4 || xmlHttp.status == 0) {
         var name = document.all("Login").value;
-        xmlHttp.open('GET', "RegScript.php?login=" + name, true);
-        xmlHttp.onreadystatechange = ResponseHandler;
-        xmlHttp.send(null);
+        xmlHttp.open('GET', "RegPHP.php?login=" + name, true);
+        console.log('>>' + name);
+        if (name) {
+            xmlHttp.onreadystatechange = ResponseHandler;
+            xmlHttp.send(null);
+        } else {
+            setTimeout("request()", 1000);
+        }
     } else {
         setTimeout("request()", 1000);
     }
@@ -15,6 +20,14 @@ function request () {
 
 function ResponseHandler() {
     if (xmlHttp.readyState == 4) {
-
+        if (xmlHttp.status == 200) {
+            xmlResponse = xmlHttp.responseXML;
+            XMLDocumentElement = xmlResponse.documentElement;
+            Response = XMLDocumentElement.firstChild.data;
+            document.all("message").innerHTML = '<i>' + Response + '</i.>';
+            setTimeout("request();", 1000);
+        } else {
+            alert("Ошибка доступа к серверу");
+        }
     }
 }
