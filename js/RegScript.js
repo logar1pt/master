e966ee3c436565ec943ccea49a49ef29ac11d8f8
Logar1pt <osokin@gmail.com>
2016-05-30 22:05:18 +0400

    Add pic, php

--- js/RegScript.js
+++ js/RegScript.js
@@ -2,32 +2,13 @@ var xmlHttp = new XMLHttpRequest();
 if (!xmlHttp) {
     alert('Error creating the XMLHttpRequest');
 }
-function request () {
-    if (xmlHttp.readyState == 4 || xmlHttp.status == 0) {
-        var name = document.all("Login").value;
-        xmlHttp.open('GET', "RegPHP.php?login=" + name, true);
-        console.log('>>' + name);
-        if (name) {
-            xmlHttp.onreadystatechange = ResponseHandler;
-            xmlHttp.send(null);
-        } else {
-            setTimeout("request()", 1000);
-        }
-    } else {
-        setTimeout("request()", 1000);
-    }
-}
 
-function ResponseHandler() {
-    if (xmlHttp.readyState == 4) {
-        if (xmlHttp.status == 200) {
-            xmlResponse = xmlHttp.responseXML;
-            XMLDocumentElement = xmlResponse.documentElement;
-            Response = XMLDocumentElement.firstChild.data;
-            document.all("message").innerHTML = '<i>' + Response + '</i.>';
-            setTimeout("request();", 1000);
-        } else {
-            alert("Ошибка доступа к серверу");
+function request(str) {
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            document.getElementById("LoginError").innerHTML = xmlHttp.responseText;
         }
-    }
+    };
+    xmlHttp.open("GET", "http://127.0.0.1:8080/master/php/RegPHP.php?q=" + str, true);
+    xmlHttp.send();
 }
--- php/RegPHP.php
+++ php/RegPHP.php
@@ -1,11 +1,57 @@
-<php ?
-header('Content-Type: text/xml');
-echo '<?xml version="1.0" ?>';
-echo '<responce>';
-$login = $_GET['login'];
-$userNames = array('PETYA', 'SERGEY', 'IVAN', 'MASHA', 'LENA');
-if (in_array(strtoupper($login), $userNames)) echo 'Hello user' + $login + '!';
-else if (trim($login) == '') echo 'Empty login';
-else echo $login + ' - unreginstered user';
-echo '</response>';
+<?php
+// Array with names
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Nika";
+$a[] = "Ophelia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
+
+// get the q parameter from URL
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+// lookup all hints from array if $q is different from "" 
+if ($q !== "") {
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+
+// Output "no suggestion" if no hint was found or output correct values 
+echo $hint === "" ? "no suggestion" : $hint;
 ?>
