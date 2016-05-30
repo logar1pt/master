08742a3ddbe199fe00f75c6375158d4ce9e6353d
Logar1pt <osokin@gmail.com>
2016-05-30 23:54:18 +0400

    I have a plan

--- js/RegScript.js
+++ js/RegScript.js
@@ -3,12 +3,12 @@ if (!xmlHttp) {
     alert('Error creating the XMLHttpRequest');
 }
 
-function request(str) {
+function request() {
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            document.getElementById("LoginError").innerHTML = xmlHttp.responseText;
+            document.getElementById("peop").innerHTML = xmlHttp.responseText;
         }
     };
-    xmlHttp.open("GET", "http://127.0.0.1:8080/master/php/RegPHP.php?q=" + str, true);
+    xmlHttp.open("GET", "http://127.0.0.1:8080/master/php/RegPHP.php?login=" + $j("#Login").val() + "&email=" + $j("#email").val(), true);
     xmlHttp.send();
 }
