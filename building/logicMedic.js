6d971762862f4b81cc5b8cd5edc69315903fe337
Logar1pt <osokin@gmail.com>
2016-07-04 23:58:49 +0300

    change logic medic

--- building/logicMedic.js
+++ building/logicMedic.js
@@ -1,8 +1,22 @@
 ﻿var $j = jQuery.noConflict();
+var maxPeople;
+var healthPeople;
+var sickPeople;
+var getInfo = function () {
+    maxPeople = $j('#maxPeople').val();
+    healthPeople = $j('#healthPeople').val();
+    sickPeople = $j('#sickPeople').val();
+}
+var setInfo = function () {
+    $j('#maxPeople').val(maxPeople);
+    $j('#healthPeople').val(healthPeople);
+    $j('#sickPeople').val(sickPeople);
+}
 var startVirus = function () {
-    setTimeout(changePeople, 3000);
+    getInfo();
+
+    setInfo();
 }
 var changePeople = function () {
-    var addPeople = $j('#maxPeople').val();
-    $j('#maxPeople').val(addPeople*1.1);
+    
 }
