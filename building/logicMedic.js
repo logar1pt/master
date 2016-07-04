6438112a23fcc9468401641384764bfc7ecef6b9
Logar1pt <osokin@gmail.com>
2016-07-04 23:51:28 +0300

    Add logic for medic house

---
+++ building/logicMedic.js
@@ -0,0 +1,8 @@
+﻿var $j = jQuery.noConflict();
+var startVirus = function () {
+    setTimeout(changePeople, 3000);
+}
+var changePeople = function () {
+    var addPeople = $j('#maxPeople').val();
+    $j('#maxPeople').val(addPeople*1.1);
+}
