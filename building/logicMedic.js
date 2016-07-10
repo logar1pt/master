5353ed0d7ad1090b93b0eed83fe6f3995e98ce61
Kvanter <osokin@gmail.com>
2016-07-10 18:55:45 +0300

    Change logic for medic house

--- building/logicMedic.js
+++ building/logicMedic.js
@@ -1,22 +1,73 @@
 ﻿var $j = jQuery.noConflict();
-var maxPeople;
-var healthPeople;
-var sickPeople;
-var getInfo = function () {
-    maxPeople = $j('#maxPeople').val();
-    healthPeople = $j('#healthPeople').val();
-    sickPeople = $j('#sickPeople').val();
+var population = {};
+var buildings = {};
+function init() {
+	population["maxPeople"] = getMaxPeople();
+	population["healthPeople"] = getHealthPeople();
+	population["sickPeople"] = getSickPeople();
+	buildings["allBuildings"] = getAllBuildings();
+	buildings["operBuildings"] = getOperBuildings();
+	buildings["pBuildings"] = getPBuildings();
 }
-var setInfo = function () {
-    $j('#maxPeople').val(maxPeople);
-    $j('#healthPeople').val(healthPeople);
-    $j('#sickPeople').val(sickPeople);
+function getMaxPeople () {
+	return $j('#maxPeople').val();
 }
-var startVirus = function () {
-    getInfo();
-
-    setInfo();
+function getHealthPeople () {
+	return $j('#healthPeople').val();
 }
-var changePeople = function () {
-    
+function getSickPeople () {
+	return $j('#sickPeople').val();
+}
+function getAllBuildings () {
+	return $j('#allBuildings').val();
+}
+function getOperBuildings () {
+	return $j('#operBuildings').val();
+}
+function getPBuildings () {
+	return $j('#pBuildings').val();
+}
+function check () {
+	init();
+	for (var inp in population) {
+		console.log('population['+inp+']>>'+population[inp]);
+	}
+	setAllPeople();
+	for (var inp in population) {
+		console.log('population['+inp+']>>'+population[inp]);
+	}
+}
+function setAllPeople() {
+	function setMaxPeople () {
+		$j('#maxPeople').val(population["maxPeople"]);
+	}
+	function setHealthPeople () {
+		$j('#healthPeople').val(population["healthPeople"]);
+	}
+	function setSickPeople () {
+		$j('#sickPeople').val(population["sickPeople"]);
+	}
+}
+function setAllBuilding () {
+	function setAllBuilding () {
+		$j('#allBuildings').val(buildings["allBuildings"]);
+	}
+	function setOperBuldings () {
+		$j('#operBuilding').val(building["operBuildings"]);
+	}
+	function setPBuildings () {
+		$j('#pBuildings').val(buildings["pBuildings"]);
+	}
+}
+function changeMaxPeople (znach) {
+	population["maxPeople"] = znach;
+}
+function changeHealthPeople (znach) {
+	population["healthPeople"] = znach;
+}
+function changeSickPeople (znach) {
+	population["sickPeople"] = znach;
+}
+function startVirus () {
+	
 }
